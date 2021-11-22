eed7d768eecea4726e125f7699a20b943ad8ffe9
Demian Panassol <demianpanassol01@gmail.com>
2021-11-22 11:45:02 -0300

    teste de config

--- public/js/addCart.js
+++ public/js/addCart.js
@@ -1,27 +1,32 @@
 document.querySelectorAll('#addCarrinho').forEach(form => {
     form.addEventListener('submit', async(e) => {
         e.preventDefault();
-        const response = await axios.get(`/carrinho/adicionar/${form.children[0].value}`);
-        const { nomeProduto, newProduct } = response.data;
+        try {
+            const response = await axios.get(`/carrinho/adicionar/${form.children[0].value}`);
+            const { nomeProduto, newProduct } = response.data;
+            if (newProduct) {
+                new Msg(`${nomeProduto} foi adicionado ao carrinho`).sucessMsg();
+            } else {
+                new Msg(`${nomeProduto} já foi adicionado ao carrinho`).errorMsg();
+            };
 
-        if (newProduct) {
-            new Msg(`${nomeProduto} foi adicionado ao carrinho`).sucessMsg();
-        } else {
-            new Msg(`${nomeProduto} já foi adicionado ao carrinho`).errorMsg();
-        };
-
-        let btns = document.querySelectorAll('.btn-close-white');
-        for (let i = 0; i < btns.length; i++) {
-            let toast = btns[i].parentNode.parentNode;
-            btns[i].addEventListener('click', () => {
-                toast.classList.remove("show");
-            });
-
-            setTimeout(() => {
-                if (toast.classList.contains('show')) {
+            let btns = document.querySelectorAll('.btn-close-white');
+            for (let i = 0; i < btns.length; i++) {
+                let toast = btns[i].parentNode.parentNode;
+                btns[i].addEventListener('click', () => {
                     toast.classList.remove("show");
-                }
-            }, 3750);
-        };
+                });
+
+                setTimeout(() => {
+                    if (toast.classList.contains('show')) {
+                        toast.classList.remove("show");
+                    }
+                }, 3750);
+            };
+        } catch (error) {
+            console.log(error);
+        }
+
+
     });
 });
